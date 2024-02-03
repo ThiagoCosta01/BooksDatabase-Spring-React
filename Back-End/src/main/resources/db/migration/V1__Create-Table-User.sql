0c1b75ad4309c2055cdf9ab723534a3bba5d39d7
Thiago Costa <thiago10lscbf@gmail.com>
2024-02-03 12:06:22 -0300

    Databases Created

---
+++ Back-End/src/main/resources/db/migration/V1__Create-Table-User.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tb_user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    userpassword VARCHAR(255) NOT NULL,
+    sex ENUM('M', 'F', 'O', 'UNKNOWN') DEFAULT 'UNKNOWN',
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
